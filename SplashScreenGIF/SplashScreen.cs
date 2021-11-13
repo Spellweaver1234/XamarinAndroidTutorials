576b8075d05303fc419ef4b16a1ba661e677ac33
Arseniy Zorin <arsenchozzz@hotmail.com>
2021-11-14 00:17:29 +0300

    Add project files.

---
+++ SplashScreenGIF/SplashScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Timers;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+using AndroidX.AppCompat.App;
+
+using Felipecsl.GifImageViewLibrary;
+
+namespace SplashScreenGIF
+{
+    [Activity(Label = "SplashScreen", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class SplashScreen : AppCompatActivity
+    {
+        private GifImageView gifImageView;
+        private ProgressBar progressBar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SplashScreen);
+
+            gifImageView = FindViewById<GifImageView>(Resource.Id.gifImageView);
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+
+            Stream input = Assets.Open("splash_screen.gif");
+            byte[] bytes = ConvertFileToByteArray(input);
+            gifImageView.SetBytes(bytes);
+            gifImageView.StartAnimation();
+
+            var timer = new Timer();
+            timer.Interval = 3000;
+            timer.AutoReset = false;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            StartActivity(new Intent(this, typeof(MainActivity)));
+        }
+
+        private byte[] ConvertFileToByteArray(Stream input)
+        {
+            byte[] buffer = new byte[16*1024];
+            using (var ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    ms.Write(buffer, 0, read);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ ToolBar/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace ToolBar
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            var inflater = MenuInflater;
+            inflater.Inflate(Resource.Menu.toolbar, menu);
+
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            switch (id)
+            {
+                case Resource.Id.email:
+                    Toast.MakeText(this, "Email", ToastLength.Short).Show();
+                    break;
+                case Resource.Id.share:
+                    Toast.MakeText(this, "Share", ToastLength.Short).Show();
+                    break;
+                case Resource.Id.search:
+                    Toast.MakeText(this, "Search", ToastLength.Short).Show();
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
